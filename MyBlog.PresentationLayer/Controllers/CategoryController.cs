ef57d1886b15acfe29ce4f743f89b40b1c9930b5
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-20 20:50:39 +0300

    category

--- MyBlog.PresentationLayer/Controllers/CategoryController.cs
+++ MyBlog.PresentationLayer/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
+using Newtonsoft.Json.Linq;
 
 namespace MyBlog.PresentationLayer.Controllers
 {
@@ -14,8 +16,41 @@ namespace MyBlog.PresentationLayer.Controllers
 
         public IActionResult CategoryList()
         {
-            var values =_categoryService.TGetAll();
+            var values = _categoryService.TGetAll();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return RedirectToAction("CategoryList");
+
+        }
+
+        public IActionResult DeleteCategory(int id)           
+        {
+            _categoryService.TDelete(id);
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category) 
+        {
+            _categoryService.TUpdate(category);
+            return RedirectToAction("CategoryList");
         }
     }
 }
