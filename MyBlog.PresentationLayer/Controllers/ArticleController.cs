0431f50ccb428933d86b22f5c3d98751ca72c2bb
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-21 22:00:16 +0300

    ViewComponents eklendı

--- MyBlog.PresentationLayer/Controllers/ArticleController.cs
+++ MyBlog.PresentationLayer/Controllers/ArticleController.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
 
 namespace MyBlog.PresentationLayer.Controllers
 {
     public class ArticleController : Controller
     {
         private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
 
-        public ArticleController(IArticleService articleService)
+        public ArticleController(IArticleService articleService, ICategoryService categoryService)
         {
             _articleService = articleService;
+            _categoryService = categoryService;
         }
 
         public IActionResult ArticleList()
@@ -17,5 +21,31 @@ namespace MyBlog.PresentationLayer.Controllers
             var values = _articleService.TArticleListWithCategory();
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult CreateArticle()
+        {
+            var categoryList = _categoryService.TGetAll();
+            List<SelectListItem>values1=(from x in categoryList select new SelectListItem{
+                Text=x.CategoryName,
+                Value=x.CategoryId.ToString()
+            }).ToList();
+            ViewBag.v1 = values1;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateArticle(Article article)
+        {
+            article.CreateDate = DateTime.Now;
+            _articleService.TInsert(article);
+            return RedirectToAction("ArticleList");
+        }
+
+        public IActionResult DeleteArticle(int id)
+        {
+            _articleService.TDelete(id);
+            return RedirectToAction("ArticleList");
+        }
+
     }
 }
